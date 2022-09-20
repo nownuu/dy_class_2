f8275b203593762e871e372e51a634c5c6b4c36f
nownuu <nownuu98@naver.com>
2022-09-20 17:42:27 +0900

    0906_첫 번째 java 수업
    hello world 출력

---
+++ day01/src/hello/Helloworld.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		
+		//학번 이름 출력하기
+		System.out.println("20221021 박소현");
+		
+		//30살에는 00을 할 것입니다. 출력하기
+		System.out.println("30살에는 직장을 가질 것 입니다.");
+	}
+}
