079e8e1c2012f32cb1251651b3109a1503aec4c5
nownuu <nownuu98@naver.com>
2022-10-11 23:36:11 +0900

    1011_다섯 번째 수업(+2차원 배열)
    2차원 배열 이론만

---
+++ day05/src/dy1011/Array05.java
@@ -0,0 +1,26 @@
+package dy1011;
+/*
+ * 2차원 배열
+ * int arr[][] = new int[5][10];
+ * int size1 = arr.length; //5
+ * int size2 = arr[0].length; //10 -> 0번째 행의 열의 개수로서 10
+ * int size3 = arr[1].length; //10 -> 1번째 행의 열의 개수로서 10
+ * 
+ * 중첩 반복
+ *  = 반복문이 다른 반복문을 내포하는 구조
+ * 
+ * 비정방형 배열
+ *  = 각 행의 열의 개수가 같은 배열 
+ *  	int i [][];
+ *  	i = new int[4][];
+ *  	i[0] = new int [1];
+ *  	i[1] = new int [2];
+ *  	i[2] = new int [3];
+ *   */
+
+public class Array05 {
+	public static void main(String[] args) {
+		// 학년별로 1학기 2학기 성적을 저장하고 4년간 전체 평점 평균을 출력
+
+	}
+}
